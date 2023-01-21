843bc67859d4fe20cb72e9d234084443dd54554b
pawowczar <pawelowczarek82@gmail.com>
2023-01-21 22:29:24 +0100

    Modify js Add Class Active on click

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+    clickedElement.classList.add('active');
+   
+  
+  
+  console.log('clickedElement:', clickedElement);
+  
+  /* [DONE] remove class 'active' from all articles */
 
-  
-  /* remove class 'active' from all articles */
-  
   const activeArticles = document.querySelectorAll('.post');
 
   for(let activeArticle of activeArticles){
