8cce80ff23d96cef3bba5ad50c016888a81c925c
pawowczar <pawelowczarek82@gmail.com>
2023-01-22 20:37:43 +0100

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -28,11 +28,19 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector ' + articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle '+ targetArticle);
+  
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+
 }
+
 
 const links = document.querySelectorAll('.titles a');
 
